f2c6ba6e8c0a130d54551868973f9347b5ec1efb
Robin <robinkaris7@gmail.com>
2019-09-01 16:48:07 +0300

    this commit will add variable instances

--- user.py
+++ user.py
@@ -1,7 +1,22 @@
 class User:
- """
- class that generates new instnaces of  users
- """
+    """
+    class that generates new instnaces of  users
+    """
+    User_list=[]
 
- 
-pass    
+   
+
+
+    def __init__ (self,User_name,password,email,account_name):
+        """
+        __init__ method helps us to define properties of our objects.
+        Args:
+             account_name: New user account name.
+             User_name: New user name.
+             password: New user password.
+             email: New user email.
+        """
+        self.account_name: account_name
+        self.User_name: User_name
+        self.password: password
+        self.email: email     
